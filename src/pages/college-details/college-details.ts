cab6ac14c1c4c78cef36a8669fe7837b7575fbdd
Ashok Tamang <ashoktamang52@gmail.com>
2016-12-01 01:53:38 -0500

    feat: changes

--- src/pages/college-details/college-details.ts
+++ src/pages/college-details/college-details.ts
@@ -71,7 +71,7 @@ export class CollegeDetailsPage {
         'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum magna nunc, scelerisque malesuada tempor ac, ultricies scelerisque enim. Vestibulum non neque quam. Ut pulvinar mollis placerat. Aliquam ac ultricies nulla. Mauris ac mattis nisi. Curabitur quis hendrerit tellus. Ut justo justo, auctor blandit interdum at, aliquam at odio. Pellentesque sagittis tortor id arcu viverra, a dictum nisi venenatis. Integer sapien mi, feugiat vitae efficitur quis, mattis quis neque. Nam efficitur consequat sem, vitae tristique purus dapibus id. Maecenas eleifend, ex vitae semper mollis, eros erat auctor orci, sit amet ullamcorper arcu nibh eu risus. In erat magna, euismod eget suscipit sit amet, ornare at lorem. Proin vehicula porta metus non dignissim. Nam enim libero, eleifend ac blandit ac, dictum ac libero. Proin eu condimentum arcu, nec varius lorem. Sed elementum vulputate velit id accumsan. Nullam vel leo eu libero blandit varius suscipit ut magna. Quisque quis ornare sem. Nam sit amet pulvinar neque. Duis quis commodo metus, eget vehicula justo. Nunc dapibus neque sed urna ultricies fringilla. Sed varius leo sed enim consectetur aliquam. Sed non odio nibh. Nulla libero turpis, congue quis neque et, tristique fermentum orci. Donec venenatis pharetra nibh quis venenatis.'
       }
     ];
-    this.college = colleges[this.params.get('index')];
+    this.college = this.params.get('college');
   }
 
   goDonate(item) {
